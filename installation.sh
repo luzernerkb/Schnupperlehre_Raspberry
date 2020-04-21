b29ab56fdf961824eac25d5640eed1a4363ee59d
ErinBachmann <57983942+EriN-B@users.noreply.github.com>
2020-04-21 16:30:58 +0200

    Changing the name of a directory

--- installation.sh
+++ installation.sh
@@ -8,7 +8,7 @@ echo Benenne nun den Rasperry pi, so wie du ihn willst.;
 read Hostname
 sudo hostname $Hostname;
 ls;
-sudo mv /home/pi/Documents/Schnuppertag_Files/Scroll_Phat_IP /etc/init.d/;
+sudo mv /home/pi/Documents/Schnupperlehre/Scroll_Phat_IP /etc/init.d/;
 
 
 

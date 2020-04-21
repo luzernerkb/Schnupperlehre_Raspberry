b5d8560cf3022cb8d72cd9ba6e5f4bef7ff103df
ErinBachmann <57983942+EriN-B@users.noreply.github.com>
2020-04-21 16:29:05 +0200

    Adding "ls"

--- installation.sh
+++ installation.sh
@@ -7,7 +7,7 @@ git clone https://github.com/luzernerkb/Schnupperlehre_Raspberry.git Schnupperle
 echo Benenne nun den Rasperry pi, so wie du ihn willst.;
 read Hostname
 sudo hostname $Hostname;
-
+ls;
 sudo mv /home/pi/Documents/Schnuppertag_Files/Scroll_Phat_IP /etc/init.d/;
 
 

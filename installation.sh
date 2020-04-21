ad55eca7c25c53b1889feae65b1114cbd064db4d
ErinBachmann <57983942+EriN-B@users.noreply.github.com>
2020-04-21 15:58:10 +0200

    Adding a Clone Command

--- installation.sh
+++ installation.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+cd Documents;
+
+git clone https://github.com/luzernerkb/Schnupperlehre_Raspberry.git Schnupperlehre
+
 echo Benenne nun den Rasperry pi, so wie du ihn willst.;
 read Hostname
 sudo hostname $Hostname;

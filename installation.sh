8722d40abc377dc787b65ab26e08f1d57c7f6913
ErinBachmann <57983942+EriN-B@users.noreply.github.com>
2020-04-21 15:54:42 +0200

    Adding a Change Directory command

--- installation.sh
+++ installation.sh
@@ -1,18 +1,11 @@
 #!/bin/sh
 
-cd Documents;
-pwd;
-
-git clone https://github.com/EriN-B/Rasperry-pi Schnuppertag_Files;
-
-
 echo Benenne nun den Rasperry pi, so wie du ihn willst.;
 read Hostname
 sudo hostname $Hostname;
 
-pwd;
+sudo mv /home/pi/Documents/Schnuppertag_Files/Scroll_Phat_IP /etc/init.d/;
 
-cd Schnuppertag_Files;
 
 
 

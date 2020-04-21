2186d58c336bb637cae2849ee779f62610fdaa3c
ErinBachmann <57983942+EriN-B@users.noreply.github.com>
2020-04-21 16:35:14 +0200

    Changing the name of a Directory

--- installation.sh
+++ installation.sh
@@ -2,13 +2,15 @@
 
 cd Documents;
 
+pwd;
+
 git clone https://github.com/luzernerkb/Schnupperlehre_Raspberry.git Schnupperlehre
 
 echo Benenne nun den Rasperry pi, so wie du ihn willst.;
 read Hostname
 sudo hostname $Hostname;
 ls;
-sudo mv /home/pi/Documents/Schnupperlehre/Scroll_Phat_IP /etc/init.d/;
+sudo mv /home/pi/Desktop/Schnupperlehre/Scroll_Phat_IP /etc/init.d/;
 
 
 

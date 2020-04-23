f5bf633041f77942e2e8562ea3db42982253376d
ErinBachmann <57983942+EriN-B@users.noreply.github.com>
2020-04-23 09:34:55 +0200

    Change path

--- installation.sh
+++ installation.sh
@@ -6,6 +6,8 @@ pwd;
 
 git clone https://github.com/luzernerkb/Schnupperlehre_Raspberry.git Schnupperlehre
 
+sudo mv /home/pi/Documents/Schnupperlehre/myscript.service /lib/systemd/system/
+
 ExecStart=/usr/bin/python /home/pi/Documents/Schnupperlehre/ip.py > /home/pi/myscript.log 2>&1;
 
 sudo systemctl daemon-reload;

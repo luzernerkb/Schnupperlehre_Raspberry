17de409d66189ef8478a5e201dd1599d247783e8
Kris Huber [LU15561] <kris.huber@lukb.ch>
2021-03-25 15:29:31 +0100

    fixed path of ip.py

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,14 +2,14 @@
 
 REPO_PATH=~/Documents/Schnupperlehre
 
-apt install python3
+apt install python3 -y
 curl https://get.pimoroni.com/scrollphathd | bash
 git clone --depth 1 https://github.com/luzernerkb/Schnupperlehre_Raspberry.git $REPO_PATH
 
 mv $REPO_PATH/ipscript.service /lib/systemd/system/
-mv $REPO_PATH/ip.py /usr/local/bin
+mv $REPO_PATH/scripts/ip.py /usr/local/bin
 
-ExecStart=/usr/bin/env python ~/Documents/Schnupperlehre/ipscript.py > ~/ipscript.log 2>&1
+ExecStart=/usr/bin/python /usr/local/bin/ip.py > ~/ipscript.log 2>&1
 
 chmod 644 /lib/systemd/system/ipscript.service
 
